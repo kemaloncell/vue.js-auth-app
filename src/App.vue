e08ea4f5f9d00e57f7bafb10d0f50754ded65e32
kemaloncell <57701397+kemaloncell@users.noreply.github.com>
2021-11-12 10:11:28 +0300

    create workspace

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <Header />
+    <HomePage />
+  </div>
+</template>
+<script>
+import Header from './components/Header';
+// import Auth from "./pages/auth/Auth"
+import HomePage from './pages/Homepage';
+// import About from "./pages/About"
+export default {
+  components: {
+    Header,
+    // Auth,
+    HomePage,
+    // About
+  },
+};
+</script>
---
+++ src/pages/About.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="container-fluid">
+    <div class="row mt-5">
+      <div class="col-md-6 offset-3">
+        <h3>Hakkımda Sayfası</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, repudiandae ipsa cum, sequi tempore aperiam numquam enim mollitia, laborum laboriosam in eaque quisquam veniam illo corporis placeat autem reiciendis. Iste! Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit dolorum placeat numquam odit dolor harum minima, dolore illo, dolorem cupiditate quod mollitia similique
+          vero temporibus tempore assumenda blanditiis nulla? Natus. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perferendis distinctio quibusdam recusandae dolorem illo facilis magni dolorum blanditiis debitis cumque, nulla ipsa harum et nisi, veniam eos aspernatur similique ratione! 🙂
+        </p>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos expedita reprehenderit neque maiores fugit quos. Enim, officiis nobis expedita vero sit voluptatem nisi at quas! Debitis aliquid rerum mollitia nulla. Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis corrupti expedita, alias ipsa quasi, porro distinctio obcaecati dolorum suscipit pariatur enim sed nihil
+          minus itaque dolorem ad molestiae iure optio! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni est a quasi dolor cum suscipit deserunt, doloremque aut error odio repudiandae fuga tenetur neque voluptatibus, delectus, libero mollitia aspernatur omnis! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae velit laboriosam suscipit magni vero! Sapiente
+          reiciendis delectus itaque fugiat, repellendus quaerat facere quas neque sed maxime. Distinctio est numquam laborum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, laboriosam aliquam eos aut laborum vero! Eveniet placeat nobis ea ipsum corrupti voluptatem natus voluptas molestiae impedit tenetur! Rem, corrupti nostrum. Lorem ipsum dolor sit amet consectetur adipisicing
+          elit. Aliquid blanditiis possimus optio, nihil rerum eius voluptatibus, aspernatur obcaecati neque perferendis quia! Aperiam eum nobis eos accusamus ipsa iusto voluptas assumenda! Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim laudantium minima excepturi vitae voluptas sint corporis ipsa tempora, nihil hic quas alias, voluptatum libero provident quis temporibus
+          exercitationem, ducimus reprehenderit? Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum culpa, illum aspernatur hic consequatur provident quis tempora quibusdam at ipsam, adipisci autem! At totam nihil laboriosam! Autem quos illum impedit.
+        </p>
+
+        <h4>Lorem ipsum dolor sit amet consectetur</h4>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nam tempora quasi asperiores tempore reprehenderit aperiam ullam molestias earum aspernatur veritatis harum, odio quae vel ipsum voluptate consequuntur rem. Aliquid, ea. Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem quidem, nesciunt illo, dolorum asperiores nisi, nam ullam vero iure in deleniti! Praesentium
+          dignissimos repellendus voluptatibus quaerat officiis quidem dicta adipisci?
+        </p>
+
+        <h4>Lorem ipsum dolor sit amet consectetur</h4>
+
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id ab suscipit reprehenderit, maxime maiores, officia architecto quia porro provident accusamus iste cumque assumenda quaerat voluptatem fugit aliquid, doloribus eos adipisci. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto atque, harum sunt, vel aut tempora perferendis pariatur itaque ipsum molestiae veritatis
+          eligendi illo dicta nemo et tenetur dolore fugiat cumque.
+        </p>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque neque veritatis aspernatur similique doloribus dolore sit quos a, sed, repellat accusamus eligendi ex! Excepturi quasi ipsum dolorem, debitis modi adipisci.</p>
+
+        <h4>Lorem, ipsum dolor sit</h4>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam ad,</p>
+        <h4>Lorem ipsum dolor sit amet</h4>
+        <blockquote>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam ad.</blockquote>
+      </div>
+    </div>
+  </div>
+</template>
