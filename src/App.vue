0bf8a94712cec40da6986f07e84cf3f5ef437fb0
kemaloncell <57701397+kemaloncell@users.noreply.github.com>
2021-11-14 13:09:02 +0300

    Define localStorge and ensure token value is valid on page refresh

--- src/App.vue
+++ src/App.vue
@@ -11,5 +11,8 @@ export default {
   components: {
     Header,
   },
+  created() {
+    this.$store.dispatch('initAuth');
+  },
 };
 </script>
--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import axios from 'axios';
+import { router } from '../router/router';
 Vue.use(Vuex);
 
 const store = new Vuex.Store({
@@ -17,6 +18,17 @@ const store = new Vuex.Store({
     },
   },
   actions: {
+    //this function is executed by the created method in app.vue
+    initAuth({ commit }) {
+      //set token to localStorge on page refresh
+      let token = localStorage.getItem('token');
+      if (token) {
+        commit('setToken', token);
+        router.push('/');
+      } else {
+        return false;
+      }
+    },
     login({ commit }, authData) {
       let authLink = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=';
 
@@ -26,12 +38,16 @@ const store = new Vuex.Store({
 
       return axios.post(authLink + 'AIzaSyBWMgJ1KHSYNFwbQp_Tk8vYCX4Yd-NpwhM', { email: authData.email, password: authData.password, returnSecureToken: true }).then((res) => {
         commit('setToken', res.data.idToken);
+        //We set the token to localStorge
+        localStorage.setItem('token', res.data.idToken);
       });
     },
     logout({ commit, state }) {
       // I ran the logout method in the store with dispatch
       commit('clearToken', state.token);
-      this.$router.replace('/');
+
+      // delete data with id "token" from localStorge
+      localStorage.removeItem('token');
     },
   },
   getters: {
